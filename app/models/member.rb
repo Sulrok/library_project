5c8530cbb2aa55ee196ca3028ef11268d1672b94
sulrok <rom1.bonnot@gmail.com>
2016-05-06 18:03:56 +0000

    add signup button

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,6 +2,9 @@ class Member < ActiveRecord::Base
     before_save { self.email = email.downcase }
     validates :surrName,  presence: true, length: { maximum: 50, minimum: 2 }
     validates :name,  presence: true, length: { maximum: 50, minimum: 2 }
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+    has_secure_password
     validates :password,  presence: true, length: { maximum: 25, minimum: 6 }
     validates :addr1,  presence: true, length: { maximum: 100, minimum: 10 }
     validates :addr2,  presence: true, length: { maximum: 100, minimum: 10 }
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   get  'home' => 'static_pages#home'
   get  'about' => 'static_pages#about'
   get  'contact' => 'static_pages#contact'
+  get    'signup'  => 'members#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
