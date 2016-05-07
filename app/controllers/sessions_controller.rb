ba75152c3ba6edcdc2f6f0b1a2df2a06e6e4100c
sulrok <rom1.bonnot@gmail.com>
2016-05-07 17:53:40 +0000

    redirect logoin -> session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     member = Member.find_by(email: params[:session][:email].downcase)
     if member && member.authenticate(params[:session][:password])
-      # Log the user in and redirect to the user's show page.
+      log_in member
+      redirect_to member
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,12 @@
 module SessionsHelper
+
+  # Logs in the given user.
+  def log_in(member)
+    session[:member_id] = member.id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_member
+    @current_member ||= Member.find_by(id: session[:member_id])
+  end
 end
