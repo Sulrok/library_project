17e33e7cbb1e68161df96de905cc0b70e3497058
sulrok <rom1.bonnot@gmail.com>
2016-05-07 10:21:20 +0000

    login bug passwd

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,10 +4,11 @@ class Member < ActiveRecord::Base
     validates :name,  presence: true, length: { maximum: 50, minimum: 2 }
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
-    has_secure_password
-    validates :password,  presence: true, length: { maximum: 25, minimum: 6 }
     validates :addr1,  presence: true, length: { maximum: 100, minimum: 10 }
     validates :addr2,  presence: true, length: { maximum: 100, minimum: 10 }
     validates :tel,  presence: true, length: { maximum: 14, minimum: 10 }
     validates :userName,  presence: true, length: { maximum: 50, minimum: 5 }
+    has_secure_password
+    validates :password,  presence: true, length: { minimum: 6 }
+ 
 end
