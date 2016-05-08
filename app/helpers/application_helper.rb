0cc458c5bdb44a6d7594c801977995b18ec1aaea
Guillaume Gratigny <guillaume.gratigny@epitech.eu>
2016-05-08 12:13:20 +0000

    design/is available

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,15 @@ module ApplicationHelper
       page_title + " | " + base_title
     end
   end
+  
+  def get_book_stat(n)
+    case n
+      when 0
+          "Available"
+      when 1
+          "On loan"
+      when -1
+          "UnAvailable"
+    end
+end
 end
