251025cb5c0d66b9dfc32744f8e80dfff9199408
sulrok <rom1.bonnot@gmail.com>
2016-05-08 21:23:20 +0000

    add validate range

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ActiveRecord::Base
     validates :title,  presence: true, length: { maximum: 100}
     validates :isbn,  presence: true, length: { maximum: 100}
     validates :description,  presence: true, length: { maximum: 200}
+    validates :isAvailable,  presence: true, :numericality => { :greater_than_or_equal_to => -1, :less_than_or_equal_to => 1 }
     validates :author,  presence: true, length: { maximum: 100}
     def self.search(search)
         where("title LIKE ? OR isbn LIKE ? OR author LIKE ?", "%#{search}%", "%#{search}%", "%#{search}%") 
