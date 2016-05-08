e7cdc6a411a4533281b0d8391408789a2f7e4227
sulrok <rom1.bonnot@gmail.com>
2016-05-08 00:02:19 +0000

    show + new

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ActiveRecord::Base
+    validates :bookID,  presence: true, length: { maximum: 10}
+    validates :title,  presence: true, length: { maximum: 100}
+    validates :isbn,  presence: true, length: { maximum: 100}
+    validates :description,  presence: true, length: { maximum: 200}
+    validates :author,  presence: true, length: { maximum: 100}
 end
