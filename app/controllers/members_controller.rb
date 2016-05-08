aca8e0fdc3ad6211b86553276d0940bc309fa64a
Guillaume Gratigny <guillaume.gratigny@epitech.eu>
2016-05-08 18:18:15 +0000

    login after sign up

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -32,6 +32,7 @@ class MembersController < ApplicationController
       if @member.save
         format.html { redirect_to list_path, notice: 'Member was successfully created.' }
         format.json { render :show, status: :created, location: @member }
+        log_in @member
       else
         format.html { render :new }
         format.json { render json: list_paht.errors, status: :unprocessable_entity }
